66ea87a70b19a4de5d8f6f51b949d47116e45020
Jordinia <rizky.awanta@gmail.com>
2025-08-27 06:14:08 +0700

    Comments

--- app/models/query_models.py
+++ app/models/query_models.py
@@ -1,10 +1,20 @@
+
+# Pydantic models for /query endpoint request and response schema
 from pydantic import BaseModel
 from typing import Literal, Union
 
+
 class QueryIn(BaseModel):
+    """
+    Pydantic Request model for /query endpoint.
+    """
     query: str
 
+
 class QueryOut(BaseModel):
+    """
+    Pydantic Response model for /query endpoint.
+    """
     query: str
     tool_used: Literal["weather", "math", "llm"]
     result: Union[str, float, int]
